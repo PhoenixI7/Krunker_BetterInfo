5629ce320532159abbef97567a31abe14a24f130
PhoenixI7 <37677140+PhoenixI7@users.noreply.github.com>
2020-07-13 18:20:20 -0400

    Update betterInfo.js

--- betterInfo.js
+++ betterInfo.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Krunker Change Info Button
+// @name         Krunker Better Info
 // @namespace    http://tampermonkey.net/
-// @version      v1.0.0
+// @version      v2.0.0
 // @description  Get Info Faster
 // @author       Phoenixi7
 // @iconURL      https://phoenixpwn.com/phoenix.png
@@ -36,6 +36,108 @@
 
     function setInfoButton(i) {
         let marketId = String(document.getElementById('itemCardinventory_' + i).querySelector('.cardActions').getElementsByClassName('cardAction')[1].onclick).split('(')[2].split(',')[0];
-        window.open('https://krunker.io/social.html?p=market&i=' + marketId,'popup','width=400,height=600')
+        var win = window.open('https://krunker.io/social.html?p=market&i=' + marketId,'popup','width=400,height=900');
+        editPopup(win, i);
+    }
+
+    async function editPopup(win, i) {
+        if (win.document.getElementById('navBar')) {
+            win.document.getElementById('navBar').style.display = "none";
+            win.document.getElementsByClassName('leaderNav')[1].style.display = "none";
+            window.setTimeout(function() {
+                returnCost(win, i)
+            }, 500);
+        } else {
+            await timer(10);
+            editPopup(win, i);
+        }
+    }
+
+    async function returnCost(win, i) {
+        if (win.document.getElementById('itemCardmarket_0')) {
+            var price = win.document.getElementById('itemCardmarket_0').getElementsByClassName('marketPrice')[0].innerHTML.split('<')[0];
+            console.log(price);
+            document.getElementById('itemCardinventory_' + i).getElementsByClassName('cardEst')[0].innerHTML = price + " KR";
+            await timer(7500);
+            win.close();
+        } else if (win.document.getElementById('marketList').querySelector('div')) {
+            if (win.document.getElementById('marketList').querySelector('div').innerHTML == "No Items Found...") {
+                document.getElementById('itemCardinventory_' + i).getElementsByClassName('cardEst')[0].innerHTML = "Can't Sell";
+                win.close();
+            }
+        } else {
+            await timer(10);
+            returnCost(win, i);
+        }
+    }
+})();
+
+(function() {
+    'use strict';
+
+    function timer(ms) {
+        return new Promise(res => setTimeout(res, ms));
+    }
+
+    async function updateValue() {
+        if (document.getElementById('itemCardinventory_0')) {
+            let itemsString = document.getElementById('invTrack').innerHTML.split('<')[0];
+            let itemsInt = parseInt(itemsString);
+            let items = itemsInt + 50;
+
+            //Create Selection
+            for (let i = 0; i < items; i++) {
+                if (document.getElementById('itemCardinventory_' + i)) {
+                    let buttonOnclick = new Function(String(document.getElementById('itemCardinventory_' + i).querySelector('.cardActions').getElementsByClassName('cardAction')[0].onclick).split('{')[1].split('}')[0].split('\n')[1]);
+                    console.log(buttonOnclick);
+                    document.getElementById('itemCardinventory_' + i).querySelector('.cardActions').getElementsByClassName('cardAction')[0].onclick = function() {
+                        buttonOnclick();
+                        document.getElementsByClassName('pItemDesc')[0].innerHTML = "Getting Price..."
+                        console.log("clicked");
+                        setInfoButton(i);
+                    };
+                }
+            }
+        } else {
+            await timer(200);
+            updateValue();
+        }
+    } updateValue();
+
+    function setInfoButton(i) {
+        let marketId = String(document.getElementById('itemCardinventory_' + i).querySelector('.cardActions').getElementsByClassName('cardAction')[1].onclick).split('(')[2].split(',')[0];
+        var win = window.open('https://krunker.io/social.html?p=market&i=' + marketId,'popup','width=400,height=900');
+        editPopup(win, i);
+    }
+
+    async function editPopup(win, i) {
+        if (win.document.getElementById('navBar')) {
+            win.document.getElementById('navBar').style.display = "none";
+            win.document.getElementsByClassName('leaderNav')[1].style.display = "none";
+            window.setTimeout(function() {
+                returnCost(win, i)
+            }, 500);
+        } else {
+            await timer(10);
+            editPopup(win, i);
+        }
+    }
+
+    async function returnCost(win, i) {
+        if (win.document.getElementById('itemCardmarket_0')) {
+            var price = win.document.getElementById('itemCardmarket_0').getElementsByClassName('marketPrice')[0].innerHTML.split('<')[0];
+            document.getElementsByClassName('pItemDesc')[0].innerHTML = price + " KR";
+            await timer(100);
+            win.close();
+        } else if (win.document.getElementById('marketList').querySelector('div')) {
+            if (win.document.getElementById('marketList').querySelector('div').innerHTML == "No Items Found...") {
+                document.getElementsByClassName('pItemDesc')[0].innerHTML = "Can't Be Sold";
+                await timer(100);
+                win.close();
+            }
+        } else {
+            await timer(10);
+            returnCost(win, i);
+        }
     }
 })();
